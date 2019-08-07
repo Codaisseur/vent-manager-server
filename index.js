44fabd2ce036255f463308aad44ddaf2fa3d6fb1
David Y. Stephenson <david@davidystephenson.com>
2019-08-07 14:03:01 +0200

    Enable cors

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const ventRouter = require('./vent/router')
 const bodyParser = require('body-parser')
+const cors = require('cors')
 
 const app = express()
+
+const middleware = cors()
+app.use(middleware)
 
 const jsonParser = bodyParser.json()
 app.use(jsonParser)
